e4215bb83198154d7790c41705ea45c548c78d3f
Intelligent Sensing and Computing Laboratory <102946092+ISCLab-Bistu@users.noreply.github.com>
2023-12-21 17:15:21 +0800

    Update phi_conv2d.py
    The construction of the PB convolution kernel

--- phi_conv2d.py
+++ phi_conv2d.py
@@ -6,13 +6,15 @@ from torch import Tensor
 
 class phi_conv(nn.Conv2d):
     def __init__(self, init=True, *args, **kwargs):
-        self.a = 1
+        self.a = 1  # the propagation loss from the ring and the directional coupler
+        # r1 and r2: the coupling coefficients of the add-drop MRR
         self.r1 = 0.9
         self.r2 = 0.9
         self.init = init
         super(phi_conv, self).__init__(*args, **kwargs)
 
     def reset_parameters(self) -> None:
+        """The initialization method of the PB convolution kernels"""
         if self.init:
             nn.init.uniform_(self.weight, -0.63, -0.13)
         else:
@@ -20,23 +22,26 @@ class phi_conv(nn.Conv2d):
             nn.init.uniform_(self.weight, -torch.pi, 0)
 
     def _through(self):
+        """The output intensity of the Through port"""
         num = (self.r2 * self.a) ** 2 - 2 * self.r1 * self.r2 * self.a * torch.cos(self.weight) + self.r1 ** 2  # noqa
         denom = 1 - 2 * self.r1 * self.r2 * self.a * torch.cos(self.weight) + (self.r1 * self.r2 * self.a) ** 2  # noqa
 
         return num / denom
 
     def _dropout(self):
+        """The output intensity of the Drop port"""
         num = (1 - self.r1 ** 2) * (1 - self.r2 ** 2) * self.a
         denom = 1 - 2 * self.r1 * self.r2 * self.a * torch.cos(self.weight) + (self.r1 * self.r2 * self.a) ** 2  # noqa
 
         return num / denom
 
     def final(self):
-
+        """""The output intensity of the (Drop-Through) port""""""
         return self._dropout() - self._through()
         
 
     def forward(self, input: Tensor) -> Tensor:
+        """the construction of the PB convolution kernel"""
         w = self.final()
         out = self._conv_forward(input, w, self.bias)
 
